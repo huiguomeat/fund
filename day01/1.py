e5b08dc51eb0df3c28e337cc6e1d115a7c111984
huiguomeat <510836757@qq.com>
2019-05-12 19:36:23 +0800

    eveninglearn

--- day01/1.py
+++ day01/1.py
@@ -1,15 +1,42 @@
-fundname1 = "六禾光辉岁月1期"
-nav = 30000000
-dwjingzhi = 3
-fene = nav / dwjingzhi
-print("最新的基金份额为")
-print(nav)
-let
-memoryview
-how try:
-    
-except expression as identifier:
-    pass
-
-    how
-xiaozhangxianghuijia
+print(12 + 3 * (4 / 188))
+print(9 / 5)
+print(9 // 5)
+# 双斜杠是求整数部分，%是取余数部分 **是幂运算  True作为1，False作为0
+# int 整型 float浮点型 bool布尔型
+print(9 % 4)
+print(False + 2)
+print(1 + 1.0)
+a = 10
+print((a + 3))
+b = a + 3
+print(b)
+a = a + 1
+print(a)
+a += 1
+print(a)
+a *= 2
+print(a)
+# +=  -=意思是自加自减
+print(type(a))
+a = 12.2
+int(a)
+print(int(a))
+print(bool(0))
+a = 'xiaoming'
+print(a)
+a = '"xiaoming"'
+a = '"xiaoming" is a boy'
+print(a)
+a = '''xiaoming is a boy
+xiaoli is a girl'''
+print(a)
+# \n表示换行符号 '''用于创建多行字符串
+a = ''
+print(type(a))
+str(12.34)
+str('True')
+int('12')
+float("12.11")
+print(bool('1'))
+a = 'xiaoming is\n a girl'
+print(a)
---
+++ day01/2.py
@@ -0,0 +1,4 @@
+a = '\n xiaoli is\n a boy'
+print(a)
+fundname = '六禾光辉岁月1期'
+print(fundname, '单位净值为', 'nav')
