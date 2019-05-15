4db0395dd4534f66cc938bc0abd5de5a2a5a0cd7
huiguomeat <510836757@qq.com>
2019-05-15 09:22:39 +0800

    20190515

---
+++ day01/5str.py
@@ -0,0 +1,9 @@
+print('Hello, %s' % 'world')
+print('Hi, %s, you have $%d.' % ('Michael', 1000000))
+print('%2d-%02d' % (3, 1))
+print('%.2f' % 3.1415926)
+s1 = 72
+s2 = 85
+r = (s2 / s1 - 1) * 100
+print('%.1f%%' % r)
+# 小明成绩上升了xx.x%
